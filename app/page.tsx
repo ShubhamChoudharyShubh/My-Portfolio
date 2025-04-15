c21c7aae4fa875c5c31914b57121be41f8b25218
ShubhamChoudharyShubh <shubhamchoudharyshubh@gmail.com>
2025-04-15 10:01:22 +0530

    about text

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
         />
       </a>
       <h1 className="mb-8 text-2xl font-medium">
-  Shubham Choudhary
+  About me:
 </h1>
 <div className="prose prose-neutral dark:prose-invert">
   <p>
